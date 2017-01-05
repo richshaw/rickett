dd569d155c041aa68d01c6b199d8e7fdb6863e38
Richard Shaw <richard.shaw@bnsly.com>
2017-01-05 11:04:48 -0800

    First commit

---
+++ R/rickett.r
@@ -0,0 +1,29 @@
+#' Message
+#'
+#' This function parses messages to Rickett
+#' @param req Request from api.ai
+#' @keywords chat finance
+#' @export
+#' @examples
+#' message()
+message <- function(req) {
+  tryCatch(
+    processMessage(req),
+    error = function(e) {
+      return(conditionMessage(e))
+    }
+  )
+}
+
+#' Process Message
+#'
+#' Helper function to point request to correct parsing function
+#' @param req Request from api.ai
+processMessage <- function(req) {
+  speech <- "This is ground control to major tom"
+  list(
+    "speech" = speech,
+    "displayText" = speech,
+    "source" = "stat-server"
+  )
+}
