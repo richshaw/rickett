d68f6feac92445488c7f4e64ed7e2c7c749c34e5
Richard Shaw <richard.shaw@bnsly.com>
2017-01-05 13:12:47 -0800

    Function vars

--- R/rickett.r
+++ R/rickett.r
@@ -21,8 +21,19 @@ message <- function(...) {
 #'
 #' Helper function to point request to correct parsing function
 #' @param req Request from api.ai
-#' @importFrom jsonlite unbox
 processMessage <- function(req) {
+  func <- req$result$metadata$intentName
+  do.call(func,req)
+}
+
+## Behavior functions
+
+#' Get ticker quote
+#'
+#' Gets stock price from web service
+#' @param req Request from api.ai
+#' @importFrom jsonlite unbox
+finance.quote <- function(req) {
   speech <- "This is ground control to major tom"
   list(
     "speech" = unbox(speech),
